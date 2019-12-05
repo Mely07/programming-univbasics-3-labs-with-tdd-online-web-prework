8352f0f1d3ef8fc426b61346d9aa27baed69f188
Damely Tineo <damely.tineo07@gmail.com>
2019-12-05 02:27:23 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
-first_number = 5
-second_number = 4
-sum = 5 + 4 
-difference = 5 - 4
-product = 5 * 4
+first_number = 5;
+second_number = 4;
+sum = 5 + 4;
+difference = 5 - 4;
+product = 5 * 4;
 quotient = 5 % 4
